932b77e2ebdf8ca974cdd5e74e38507b71230471
peter enejo <petstorm101@gmail.com>
2022-06-17 12:27:24 +0100

    commit tree 113a5d6c71683858503917a1ab6fa42273d674c3

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+  
+  function moveDodgerRight() {
+    const RightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(RightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
+  
